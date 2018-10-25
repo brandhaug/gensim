2e7a95876c85eb9c6581909ad9f50d491e34eac4
brandhaug-desktop <mbrandhaug@live.no>
2018-10-25 12:32:15 +0200

    Finished

--- gensim_assignment.py
+++ gensim_assignment.py
@@ -9,23 +9,22 @@ from nltk.stem.porter import PorterStemmer
 import string
 from gensim import corpora, models, similarities
 import time
-import os.path
 
 start = time.time()
+random.seed(123)
+translator = str.maketrans('', '', string.punctuation)
+stemmer = PorterStemmer()
 
 """
 1. Data loading and preprocessing
 In this part we load and process (clean, tokenize and stem) data.
 """
-random.seed(123)
-translator = str.maketrans('', '', string.punctuation)
-stemmer = PorterStemmer()
-
 file = codecs.open("assets/pg3300.txt", "r", "utf-8")
 lines = file.readlines()
 
 collection = []
 tokenized_collection = []
+original_paragraph = ''
 paragraph = ''
 remove_paragraph = False
 
@@ -36,14 +35,20 @@ for i, line in enumerate(lines):
         if remove_paragraph:  # paragraph includes Gutenberg
             remove_paragraph = False
         else:  # add paragraph to collection
-            collection.append(paragraph)  # keep a copy of original paragraphs
+            paragraph = paragraph[:-1]  # Removes last space
+            collection.append(original_paragraph)  # keep a copy of original paragraphs
             paragraph = paragraph.translate(translator)  # remove punctuation
-            tokenized_collection.append(paragraph.split(' '))  # split paragraphs into list of words
+            paragraph = paragraph.lower()
+            tokenized_collection.append(paragraph.split())  # split paragraphs into list of words
+        original_paragraph = ''
         paragraph = ''  # reset paragraph
-    elif 'Gutenberg' in line:  # header / footer
+    elif 'gutenberg' in line.lower():  # header / footer
         remove_paragraph = True
-    else:  # add line to paragraph
-        paragraph += line_stripped
+    elif line_stripped:  # add line to paragraph
+        original_paragraph += line
+        paragraph += line_stripped + ' '
+    else:
+        pass  # new line after new line
 
 for i, paragraph in enumerate(tokenized_collection):
     for j, word in enumerate(paragraph):
@@ -98,13 +103,20 @@ lsi_model.save('assets/model.lsi')
 lsi_index = similarities.MatrixSimilarity(corpus_lsi)  # transform corpus to LSI space and index it
 lsi_index.save('assets/lsi.index')
 
+print('3.5: First 3 LSI Topics')
+topics = lsi_model.show_topics(3)
+print(*topics, sep='\n')
+
+print('\nA topic represents which words that are likely to be used in the same document')
+
+print('\n=================================')
+
 """
 4. Querying
 In this part we query the models built in the previous part and report results.
 """
-
 query = "What is the function of money?"
-print('Query:', query)
+print('\nQuery:', query)
 query = query.strip()  # remove redundant spaces etc
 query = query.translate(translator)  # remove punctuation
 query = query.split(' ')  # split paragraphs into list of words
@@ -115,40 +127,46 @@ for i, word in enumerate(query):
 query = dictionary.doc2bow(query)  # bag of words representation
 print('Preprocessed query', query)
 
-tfidf_query = tfidf_model[query]  # apply transformation to query
-print('\nTF-IDF Query', tfidf_query)
+print('\n=================================')
 
-doc2similarity = enumerate(tfidf_index[tfidf_query])
-tfidf_results = sorted(doc2similarity, key=lambda kv: -kv[1])[:3]
-print('TF-IDF Best Results', tfidf_results)
+tfidf_query = tfidf_model[query]  # apply transformation to query
+print('\nTF-IDF Query')
+
+for word in tfidf_query:
+    print(dictionary.get(word[0]), word[1])
+
+tfidf_doc2similarity = enumerate(tfidf_index[tfidf_query])
+tfidf_results = sorted(tfidf_doc2similarity, key=lambda x: x[1], reverse=True)[:3]
+print('\nTF-IDF Most Relevant Results')
 
 for i, result in enumerate(tfidf_results):
     paragraph_index = result[0]
-    print('\nResult %d - [Paragraph %d]' % (i + 1, paragraph_index))
-    print(collection[paragraph_index])
+    paragraph_score = result[1]
+    print('\nResult %d - [Paragraph %d] - %.4f' % (i + 1, paragraph_index, paragraph_score))
+    print(collection[paragraph_index][:340])
 
 print('\n=================================')
 
 lsi_query = lsi_model[tfidf_query]  # create a double wrapper over the original corpus: bow->tfidf->fold-in-lsi
-print('\nLSI Query', lsi_query)
-lsi_results = sorted(lsi_query, key=lambda kv: -abs(kv[1]))[:3]
-print('\nLSI Worst Results', lsi_results)
+print('\nLSI Top 3 Topics')
+
+lsi_topics = sorted(lsi_query, key=lambda x: abs(x[1]), reverse=True)[:3]
+
+for topic in lsi_topics:  # topics with the most significant (with the largest absolute values) weights
+    topic_index = topic[0]
+    topic_score = topic[1]
+    print('\n[Topic %d]' % topic_index)
+    print(lsi_model.show_topics()[topic_index], topic_score)
+
+lsi_doc2similarity = enumerate(lsi_index[lsi_query])
+lsi_results = sorted(lsi_doc2similarity, key=lambda x: x[1], reverse=True)[:3]
+print('\nLSI Most Relevant Results')
 
 for i, result in enumerate(lsi_results):
     paragraph_index = result[0]
-    print('\nResult %d - [Paragraph %d]' % (i + 1, paragraph_index))
-    print(collection[paragraph_index])
-
-print('\n=================================')
-
-doc2similarity = enumerate(lsi_index[lsi_query])
-lsi_results2 = sorted(doc2similarity, key=lambda kv: -kv[1])[:3]
-print('\nLSI Best Results', lsi_results2)
-
-for i, result in enumerate(lsi_results2):
-    paragraph_index = result[0]
-    print('\nResult %d - [Paragraph %d]' % (i + 1, paragraph_index))
-    print(collection[paragraph_index])
+    paragraph_score = result[1]
+    print('\nResult %d - [Paragraph %d] - %.4f' % (i + 1, paragraph_index, paragraph_score))
+    print(collection[paragraph_index][:350])
 
 end = time.time()
-print('\nExecuted in %0.4fs' % (end - start))
+print('\nExecuted in %.4fs' % (end - start))
